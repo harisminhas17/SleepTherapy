e520ec32b70da02fde171a4a703e465723431a34
Haris Minhas <harisminhas0017@gmail.com>
2025-05-22 14:57:58 +0500

    Implement getUserSleepData API and enhance sleep data retrieval functionality

---
+++ app/Models/SleepData.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SleepData extends Model
+{
+    protected $table = 'UserSleepData';
+    protected $guarded = [];
+}
